54a94b2f1ffae054b194b5915060d201a68e6194
Syecku <khusayan_syerikjan@unimedia.co.jp>
2016-11-02 16:22:37 +0800

    Implemented simple RESTful API

---
+++ main.go
@@ -0,0 +1,23 @@
+/**
+* Entry point of the system
+*
+* @author skh
+**/
+
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+
+	router := NewRouter()
+
+	log.Fatal(http.ListenAndServe(port, router))
+}
